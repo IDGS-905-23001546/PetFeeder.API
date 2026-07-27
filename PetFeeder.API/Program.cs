0589f212af6c583942015b5f95dca03c8998f870
IDGS-905-23001546 <eduardorioramirez10@gmail.com>
2026-07-26 19:12:38 -0600

    26jul26.CarlosR.Fix-PostgreSQL-port-default

--- PetFeeder.API/Program.cs
+++ PetFeeder.API/Program.cs
@@ -25,7 +25,8 @@ if (rawConnStr.StartsWith("postgresql://") || rawConnStr.StartsWith("postgres://
 {
     var uri = new Uri(rawConnStr);
     var userInfo = uri.UserInfo.Split(':');
-    connStr = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    connStr = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 else
 {
